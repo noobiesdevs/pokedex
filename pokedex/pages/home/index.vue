023804d622f1e57c9f983f8c465311ac000e010f
Robin scelso <46759103+robinScelso@users.noreply.github.com>
2023-04-06 23:08:09 +0200

    push

--- pokedex/pages/home/index.vue
+++ pokedex/pages/home/index.vue
@@ -1,19 +1,50 @@
 <template>
-    <div  v-for="n in 10">
-        <cards-pokemon/>
-    </div>
+  <div v-for="n in tablePokemon">
+    <cards-pokemon />
+  </div>
 </template>
 
-<script lang="TS" setup>
+<script lang="ts" setup>
+  let result;
+  let tablePokemon = [];
+  
+  function fetchPokemons() {
+    return fetch('https://pokeapi.co/api/v2/pokemon/?limit=5')
+    .then(response => response.json())
+    .then(data => {
+      result = data;
+      tablePokemon = data.results;
 
+      return data;
+    })
+    .catch(error => console.error(error))
+  }
+
+
+  let pokemons = fetchPokemons();
+  let test;
+  let name;
+  async function getPokemonName() {
+    try {
+      test = await pokemons
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+getPokemonName().then(() => {
+  tablePokemon = test.results
+  for (let i=0; i < 5; i++) {
+    name = test.results[i].name;
+    console.log("name : " + name)
+  }
+})
 </script>
 
-
 <style scoped>
-    .wrapper {
-    display: grid;
-    grid-template-columns: repeat(4, 300px);
-    justify-content: space-between;
-  }
+.wrapper {
+  display: grid;
+  grid-template-columns: repeat(4, 300px);
+  justify-content: space-between;
+}
 </style>
-
