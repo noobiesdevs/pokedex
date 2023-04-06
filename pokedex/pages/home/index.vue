5cb8cea71e64d7bae7983157ead62aaf83d4d253
Kami <ludovic.chevroulet@outlook.fr>
2023-04-07 00:25:28 +0200

    fix: typage error

--- pokedex/pages/home/index.vue
+++ pokedex/pages/home/index.vue
@@ -1,38 +1,36 @@
 <template>
-    <div v-for="(pokemon, i) in tablePokemon">
-        <cards-pokemon :name="namePokemon[i]" :url="urlPokemon[i]" />
+    <div v-for="(pokemon, i) in tablePokemon" :key="i">
+        <cards-pokemon :name="pokemon.name" :url="pokemon.url" />
     </div>
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue';
+
+// >===============
+// Permet de bien définir la tablePokemon afin d'enlever les erreures de typage
+// 
+// interface Pokemon {
+//     name: string;
+//     url: string;
+// }
+
+// let tablePokemon = ref<Pokemon[]>([]);
+// <===============
 let tablePokemon = ref([]);
-let namePokemon = [];
-let urlPokemon = [];
+
 async function fetchPokemons() {
     try {
         const response = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=25');
         const data = await response.json();
         tablePokemon.value = data.results;
-
-        for (let i = 0; i < tablePokemon.value.length; i++) {
-            namePokemon[i] = tablePokemon.value[i].name;
-            urlPokemon[i] = tablePokemon.value[i].url;
-        }
-
-        console.log(urlPokemon);
-
-
     } catch (error) {
         console.error(error);
     }
 }
 
-let test = "coucou"
+let test = "coucou";
 let pokemons = fetchPokemons();
-
-
-
-
 </script>
 
 <style scoped>
