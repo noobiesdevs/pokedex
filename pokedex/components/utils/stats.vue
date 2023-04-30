d1e62d60ce989063af92bc4b0d787f07283e64b7
Robin scelso <46759103+robinScelso@users.noreply.github.com>
2023-04-30 23:37:29 +0200

    Ajout des données dynamiques dans la page id vue

---
+++ pokedex/components/utils/stats.vue
@@ -0,0 +1,45 @@
+<template>
+<div class="statParent">
+    <div class="nameStat">
+        {{ nameStat }}
+    </div>
+    <div class="progressBar">
+        ***
+    </div>
+    <div class="numericValue">
+        {{ value }}
+    </div>
+
+</div>
+
+</template>
+
+<script lang="ts" setup>
+
+const props = defineProps({
+  nameStat: {
+    type: String,
+    default: ""
+  },
+  value: {
+    type: Number,
+    default: 0
+  }
+
+});
+
+</script>
+
+<style scoped>
+
+.statParent {
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+}
+
+.nameStat {
+  width: 25%;
+}
+
+</style>
--- pokedex/pages/description/[id].vue
+++ pokedex/pages/description/[id].vue
@@ -1,19 +1,60 @@
 <template>
-<img class="artwork" :src="tablePokemon.sprite" alt="pokemon-artwork"/>
-  {{ tablePokemon.name }}
-  {{ tablePokemon.height }} m
-  {{ tablePokemon.weight }} kg
-  {{ tablePokemon.hp }} hp 
-  {{ tablePokemon.attack }} attack 
-  {{ tablePokemon.defense }} defense 
-  {{ tablePokemon.specialAttack }} special-attack 
-  {{ tablePokemon.specialDefense }} special-defense 
-  {{ tablePokemon.speed }} speed 
+
+<section class="highSection">
+  <div class="divIcon">
+
+  </div>
+
+  <div class="divHeader">
+    <label class="typePokemon">{{ tablePokemon.type }}</label>
+    <h1 class="pokemonName">{{ tablePokemon.name }}</h1>
+  </div>
+
+  <div class="divTitleStats">
+    <ul>
+      <li class="height">Height</li>
+      <li class="weight">Weight</li>
+      <li class="ability">Ability</li>
+    </ul>
+  </div>
+
+  <div class="divStats">
+    <ul>
+      <li>{{ tablePokemon.height }} m</li>
+      <li>{{ tablePokemon.weight }} kg</li>
+      <li> {{ tablePokemon.ability }}</li>
+    </ul>
+      
+  </div>
+  
+  <div class="divArtwork">
+    <img class="artwork" :src="tablePokemon.sprite" alt="pokemon-artwork"/>
+  </div>
+
+</section>
+
+<section class="bottomSection">
+  <div class="stats">
+
+  </div>
+  <div class="headerStats">
+    <ul>
+      <li> <Stats nameStat="Hp" :value="tablePokemon.hp"/> </li>
+       <li> <Stats nameStat="Attack" :value="tablePokemon.attack"/> </li>
+        <li> <Stats nameStat="Defense" :value="tablePokemon.defense"/> </li>
+         <li> <Stats nameStat="Special-attack" :value="tablePokemon.specialAttack"/> </li>
+          <li> <Stats nameStat="Special-defense" :value="tablePokemon.specialDefense"/> </li>
+           <li> <Stats nameStat="Speed" :value="tablePokemon.speed"/> </li>
+    </ul>
+  </div>
+</section>
+
+
   
 </template>
 
 <script lang="ts" setup>
-
+import Stats from '@/components/utils/stats.vue'
 const route = useRoute()
 const id = ref<number>(0)
 let index
@@ -42,7 +83,9 @@ let tablePokemon = ref<Pokemon>({
   specialAttack: 0,
   specialDefense: 0,
   speed: 0,
-  sprite: ''
+  sprite: '',
+  type: ''
+
 });
 
 
@@ -66,7 +109,7 @@ async function fetchPokemons() {
     tablePokemon.value.name = data.forms[0].name
     tablePokemon.value.height = data.height/10
     tablePokemon.value.weight = data.weight/10
-    tablePokemon.value.ability = data.ability
+    tablePokemon.value.ability = data.abilities[0].ability.name
     tablePokemon.value.hp = data.stats[0].base_stat
     tablePokemon.value.attack = data.stats[1].base_stat
     tablePokemon.value.defense = data.stats[2].base_stat
@@ -74,6 +117,7 @@ async function fetchPokemons() {
     tablePokemon.value.specialDefense = data.stats[4].base_stat
     tablePokemon.value.speed = data.stats[5].base_stat
     tablePokemon.value.sprite = data.sprites.other.dream_world.front_default
+    tablePokemon.value.type = data.types[0].type.name
     
     console.log("data API", data);
     
@@ -88,4 +132,29 @@ const pokemons = fetchPokemons()
 </script> 
 
 <style scoped>
+
+.highSection {  display: grid;
+  grid-template-columns: 0.5fr 0.5fr 1fr;
+  grid-template-rows: 1fr 1fr;
+  gap: 0px 0px;
+  grid-auto-flow: row;
+  grid-template-areas:
+    "divIcon divHeader divArtwork"
+    "divTitleStats divStats divArtwork";
+}
+
+.divArtwork { grid-area: divArtwork; }
+
+.divIcon { grid-area: divIcon; }
+
+.divTitleStats { grid-area: divTitleStats; }
+
+.divStats { grid-area: divStats; }
+
+.divHeader { grid-area: divHeader; }
+
+ul {
+  list-style: none;
+}
+
 </style>
